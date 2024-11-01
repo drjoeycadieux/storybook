4f09907e8da9f2b98e9aac13708fb6693e25203b
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-11-01 18:09:40 -0400

    inm.

---
+++ src/components/HeaderNav.tsx
@@ -0,0 +1,11 @@
+export default function HeaderNav() {
+    return (
+        <div>
+            <div className="bg-gray-800 p-6 md:p-9">
+                <p className="text-white font-sans font-bold text-2xl sm:text-3xl lg:text-4xl">
+                    Security-Technologies
+                </p>
+            </div>
+        </div>
+    );
+}
