753105bd72637e94795d5a1cb822ad13b6366c9d
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-11-03 04:48:37 -0500

    inm.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             </Head>
             <div className="bg-yellow-500 text-black text-center py-2 z-20 absolute top-0 left-0 w-full">
                 <p className="font-medium">
-                    Important Update: Our website has a new look! Check it out!
+                    "Important Update: Our website has a new look! Check it out!"
                 </p>
             </div>
 
