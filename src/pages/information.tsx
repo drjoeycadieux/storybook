d89e7adb48219a21516848cb311bfb5c2a8bd184
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-11-04 18:04:10 -0500

    inm.

--- src/pages/information.tsx
+++ src/pages/information.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head';
 
 
 import Banner from '@/components/Banner';
-import Chat from '@/components/Chat';
 
 
 const InformationPage = () => {
@@ -28,7 +27,6 @@ const InformationPage = () => {
                          <h2 className="text-xl font-bold text-blue-600 mb-2">Feature 3</h2>
                          <p className="text-lg text-gray-600">Description of feature 3.</p>
                     </div>
-                    <Chat />
                </div>
           </div>
      );
