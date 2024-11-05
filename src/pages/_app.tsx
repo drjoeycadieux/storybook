81d9b0aeaa9382a592e0ed1a5e84abb29d5bbe4e
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-11-05 08:21:15 -0500

    inm.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+/**
+ * The top-most component in the app, which renders the current page.
+ *
+ * This component is special because Next.js automatically passes the
+ * `pageProps` to it, which are the props that were pre-rendered for the page.
+ *
+ * @param Component The current page component to be rendered
+ * @param pageProps The props that were pre-rendered for the page
+ */
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
